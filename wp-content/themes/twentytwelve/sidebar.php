bbfe1704bc936992ba3c2feca8c2cdfb71be9fbd
Jenny Wong <jenny@hmn.md>
2016-02-03 00:19:03 +0000

    Added initial files for site

---
+++ wp-content/themes/twentytwelve/sidebar.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * If no active widgets in sidebar, let's hide it completely.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
